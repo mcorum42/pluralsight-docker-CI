8a6d870ac072032fce1956a528d11ee795c08e7c
mcorum <mcorum42@gmail.com>
2017-01-10 09:23:32 -0500

    Initial commit

--- test/test.js
+++ test/test.js
@@ -1,13 +1,13 @@
-// Simpl async test for HTTP 200 response using supertest
+// Simple async test for HTTP 200 response code using supertest
 'use strict';
 
 var request = require("supertest"),
-	app = require("../app").getApp;
-	
+    app = require("../app").getApp;
+
 describe('GET /', function(){
-	it('expects HTTP response 200', function(done) {
-		request(app)
-			.get('/')
-			.expect(200, done);
-	});
+  it('expects HTTP response 200', function(done){
+    request(app)
+     .get('/')
+	 .expect(200, done); 
+  });
 });
